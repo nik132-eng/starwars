391e1aed8763480cc66336d74bdca69f20aae1af
Nikunj Rohit <nikunjrohit10@gmail.com>
2024-10-17 21:53:21 +0530

    try to resolve 404 error on vercel

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 import Navbar from '../src/components/Navbar';  
 import { Outlet } from 'react-router-dom';  
 import Footer from './components/Footer';
+import { HashRouter as Router } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <div>
-      <Navbar /> 
-      <Outlet />  
-      <Footer />
+        <Router>
+          <Navbar /> 
+          <Outlet />  
+          <Footer />
+        </Router>
     </div>
   );
 };
