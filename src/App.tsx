5b538577c570a5dac371e448869377bfaeaf6592
Nikunj Rohit <nikunjrohit10@gmail.com>
2024-10-17 21:55:31 +0530

    remove router for fix issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,13 @@ import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 import Navbar from '../src/components/Navbar';  
 import { Outlet } from 'react-router-dom';  
 import Footer from './components/Footer';
-import { HashRouter as Router } from 'react-router-dom';
-
 
 const App = () => {
   return (
     <div>
-        <Router>
           <Navbar /> 
           <Outlet />  
           <Footer />
-        </Router>
     </div>
   );
 };
